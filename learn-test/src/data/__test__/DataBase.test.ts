40497331a6a974f2f1a79dfa9908e7d22d6e9744
DewaSRY <sdewa6645@gmail.com>
2023-07-29 19:53:06 +0800

    done

--- learn-test/src/data/__test__/DataBase.test.ts
+++ learn-test/src/data/__test__/DataBase.test.ts
@@ -7,7 +7,7 @@ type SomeTypeWithId = {
   color: string;
 };
 
-describe("DataBase test suit data base store data as an array withe type T inisde it, also need have id on the T", () => {
+describe("DataBase test suit data base store data as an array withe type T inside it, also need have id on the T", () => {
   let suit: DataBase<SomeTypeWithId>;
   const fakeId = "1234";
   const someObject = {
@@ -30,36 +30,36 @@ describe("DataBase test suit data base store data as an array withe type T inisd
   });
   it("Database insert method insert T data type to array and return teh id", async () => {
     const actual = await suit.insert({ id: "" } as any);
-    const mockElemen = (suit as any).elements;
+    const mockElement = (suit as any).elements;
     expect(actual).toBe(fakeId);
-    expect(mockElemen).toEqual([{ id: fakeId }]);
+    expect(mockElement).toEqual([{ id: fakeId }]);
   });
   it("Database insert will pust the data type T to the pivate element", async () => {
     await suit.insert(someObject);
-    const mockElemen = (suit as any).elements;
-    expect(mockElemen).toEqual([someObject]);
+    const mockElement = (suit as any).elements;
+    expect(mockElement).toEqual([someObject]);
   });
-  it("Database getBy will looking on the stored data element the match data withe propery we put ", async () => {
+  it("Database getBy will looking on the stored data element the match data withe propers we put ", async () => {
     const id = await suit.insert(someObject);
-    const actial = await suit.getBy("id", id);
-    expect(actial).toEqual(someObject);
+    const actual = await suit.getBy("id", id);
+    expect(actual).toEqual(someObject);
   });
-  it("Database getBy will looking on the stored data element the match data withe propery we put", async () => {
+  it("Database getBy will looking on the stored data element the match data withe propers we put", async () => {
     const id = await suit.insert(someObject);
     const expectedColor = "red";
     await suit.update(id, "color", expectedColor);
     const actual = await suit.getBy("id", id);
-    expect(actual.color).toBe(expectedColor);
+    expect(actual!.color).toBe(expectedColor);
   });
   it("Database findAllBy will fond all data match the property we put", async () => {
     await suit.insert(someObject);
     await suit.insert(someObject2);
     await suit.insert(someObject2);
     const expected = [someObject2, someObject2];
-    const mockElemen = (suit as any).elements;
+    const mockElement = (suit as any).elements;
     const actual = await suit.findAllBy("color", "blue");
     expect(actual).toEqual(expected);
-    expect(mockElemen).toEqual(expect.arrayContaining(actual));
+    expect(mockElement).toEqual(expect.arrayContaining(actual));
   });
   it("Database delete will remove the match element withe the id we put", async () => {
     const id = await suit.insert(someObject);
@@ -68,7 +68,7 @@ describe("DataBase test suit data base store data as an array withe type T inisd
     const actual = await suit.getBy("id", id);
     expect(actual).toBeUndefined();
   });
-  it("Database getAllElements will returns all alament we store on Database", async () => {
+  it("Database getAllElements will returns all element we store on Database", async () => {
     await suit.insert(someObject);
     await suit.insert(someObject2);
     const expected = [someObject, someObject2];
