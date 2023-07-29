d84edfc3fbf431ada887fa793cf91ff427e6a4af
DewaSRY <sdewa6645@gmail.com>
2023-07-29 19:33:01 +0800

    iprove all change

--- learn-test/src/data/DataBase.ts
+++ learn-test/src/data/DataBase.ts
@@ -1,22 +1,24 @@
 import { generateRandomId } from "./IdGenerator";
 
-type ObjectWithId = {
+type Database = {
   id: string;
 };
-export class DataBase<T extends ObjectWithId> {
-  private elements = new Array<T>();
+
+export class DataBase<T extends Database> {
+  private elements = new Array<{ [p in keyof T]: T[p] }>();
+
   async insert(arg: T) {
     arg.id = generateRandomId();
     this.elements.push(arg);
     return arg.id;
   }
-  async getBy(argName: string, argValue: string) {
+  async getBy(argName: keyof T, argValue: string) {
     return this.elements.find((x) => x[argName] === argValue);
   }
-  async findAllBy(argName: string, argValue: string) {
+  async findAllBy(argName: keyof T, argValue: string) {
     return this.elements.filter((x) => x[argName] === argValue);
   }
-  async update(id: string, argName: string, argValue: any) {
+  async update(id: string, argName: keyof T, argValue: any) {
     const index = this.elements.findIndex((x) => x.id === id);
     this.elements[index][argName] = argValue;
   }
